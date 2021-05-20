f85e3fe3f05cd4076871b8fbafebdd945ee567ad
The Podz <stefanpodz@yahoo.com>
2021-05-20 13:49:45 -0400

    routes api.js starter code

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT, () => {
