ae3c2fc8ab138508ef259dda2d39c53f81029775
The Podz <stefanpodz@yahoo.com>
2021-05-20 19:05:46 -0400

    added index.js to models and updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,60 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.get("/api/workout", (req, res) => {
-    Workout.find()
-      .then((workoutData) => {
-        res.json(workoutData);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
-  
-  // GET request
-  router.get("/api/workout/range", (req, res) => {
-    Workout.find()
-      .then((workoutData) => {
-        res.json(workoutData);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
-  
-  // POST workout
-  router.post("/api/workout", ({ body }, res) => {
-    Workout.create(body)
-      .then((workoutData) => {
-        res.json(workoutData);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
-  
-  // Update workout
-  router.put("/api/workout/:id", ({ body, params }, res) => {
-    Workout.findOneAndUpdate(params.id, { $push: { exercises: body } })
-      .then((workoutData) => {
-        res.json(workoutData);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
-  
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((workoutData) => {
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+// GET request
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .sort({_id: -1})
+    .limit(7)
+    .then((workoutData) => {
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// POST workouts
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((workoutData) => {
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// Update workouts
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findOneAndUpdate(params.id, { $push: { exercises: body } })
+    .then((workoutData) => {
+      res.json(workoutData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
